5ba7cd03032ecf24077fc14671808cd40f94b76a
Chris Rabl <rabl.christopher@gmail.com>
2022-08-03 22:57:55 -0600

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,88 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { orderBy } from 'lodash';
+
+interface Story {
+  date: string;
+  title: string;
+  contents: string;
+  user_ulid: string;
+}
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+  <div class="app">
+    <nav>
+      <section><h3>Storyworthy</h3></section>
+    </nav>
+    <section class="spacer"></section>
+    <section class="story" *ngFor="let story of stories">
+      <h1>{{story.title}}</h1>
+      <span>{{story.date | date:'mediumDate'}} at {{story.date | date:'shortTime'}}</span>
+      <div><markdown [data]="story.contents"></markdown></div>
+    </section>
     <router-outlet></router-outlet>
+  </div>
   `,
-  styles: []
+  styles: [`
+    .app {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .spacer {
+      margin-top: 64px;
+    }
+
+    nav {
+      padding: 16px 0;
+      border-bottom: 1px solid #dadada;
+      position: fixed;
+      width: 100%;
+      background: white;
+    }
+
+    nav h3 {
+      margin: 0 0 0 16px;
+    }
+
+    section {
+      width: 100%;
+      max-width: 960px;
+      margin: 0 auto;
+      display: flex;
+    }
+
+    .story {
+      flex-direction: column;
+      padding: 0 16px;
+    }
+
+    .story h1 {
+      margin: 0.5rem 0;
+    }
+
+    .story span {
+      font-size: 0.875rem;
+      color: hsl(0deg, 0%, 40%);
+    }
+  `]
 })
 export class AppComponent {
-  title = 'storyworthy';
+  stories: Story[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  async ngOnInit() {
+    try {
+      const res = await lastValueFrom(this.http.get<Story[]>("https://crabl-storyworthy.builtwithdark.com/story"));
+      this.stories = orderBy(res, ['date'] , ['desc']);
+    } catch (err) {
+
+    }
+  }
 }
